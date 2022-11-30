eefc5dd3ccbbca178c7ce6165cfe3208a293b2e2
nekoo <nereapena42@gmail.com>
2022-11-30 18:53:31 +0100

    Empezando segunda pantalla (falta ver como almacenar los datos variables de la BBDD)

--- app/src/main/java/com/liceolapaz/des/npc/ej1npc/MainActivity2.kt
+++ app/src/main/java/com/liceolapaz/des/npc/ej1npc/MainActivity2.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity2 : AppCompatActivity() {
+    //esta clase listara todos los jugadores, y a partir de esta l
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)

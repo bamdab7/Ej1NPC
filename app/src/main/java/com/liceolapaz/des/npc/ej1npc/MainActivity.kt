ac2b0ba49c674cabd2b7d71ba3e69dfbc481c8b6
bamdab7 <nereapena42@gmail.com>
2022-11-30 16:54:02 +0100

    creando repositorio

---
+++ app/src/main/java/com/liceolapaz/des/npc/ej1npc/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.liceolapaz.des.npc.ej1npc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

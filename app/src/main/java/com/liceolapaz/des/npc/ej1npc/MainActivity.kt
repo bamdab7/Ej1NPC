5423a7337046b2f620e7e322671f211eac6278cf
nekoo <nereapena42@gmail.com>
2022-11-30 17:41:48 +0100

    Creando primera interfaz de login

--- app/src/main/java/com/liceolapaz/des/npc/ej1npc/MainActivity.kt
+++ app/src/main/java/com/liceolapaz/des/npc/ej1npc/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    //esta clase abre un formulario en el que se pide que se loggeee un usuario 'admin' con contraseña 'liceo'
+    //si se introduce otro usuario, saldra un mensaje de error. Si se introduce 3 veces mal, se sale de la app
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

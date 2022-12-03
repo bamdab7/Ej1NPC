486987a9ddfabe76c912f97584c9b17eaa3065bf
nekoo <nereapena42@gmail.com>
2022-12-03 12:41:51 +0100

    Creando base de datos vacia de los jugadores + la clase helper

---
+++ app/src/main/java/com/liceolapaz/des/npc/ej1npc/JugadoresSQLiteHelper.kt
@@ -0,0 +1,24 @@
+package com.liceolapaz.des.npc.ej1npc
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class JugadoresSQLiteHelper (
+    context : Context?,
+    name: String?,
+    factory: SQLiteDatabase.CursorFactory?,
+    version: Int) : SQLiteOpenHelper(context,name,factory,version) {
+
+    //Sentencia SQL para crear la base de datos de jugadores
+    val sqlCreate = "CREATE TABLE Jugadores (codigo INTEGER PRIMARY KEY, nombre TEXT, precio DOUBLE, posicion TEXT CHECK(posicion IN('Portero','Defensa','MedioCampista','Delantero')), puntos INTEGER)"
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        //Se ejecuta la sentencia SQL de creacion de la tabla
+        db?.execSQL(sqlCreate)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("Not yet implemented")
+    }
+}
--- app/src/main/java/com/liceolapaz/des/npc/ej1npc/MainActivity2.kt
+++ app/src/main/java/com/liceolapaz/des/npc/ej1npc/MainActivity2.kt
@@ -1,12 +1,19 @@
 package com.liceolapaz.des.npc.ej1npc
 
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity2 : AppCompatActivity() {
-    //esta clase listara todos los jugadores, y a partir de esta l
+    //esta clase listara todos los jugadores, con BBDD
+    private lateinit var db : SQLiteDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        //abrir la base de datos
+        val usdbh = JugadoresSQLiteHelper(this, "DBJugadores",null,1)
+        db = usdbh.writableDatabase
     }
 }

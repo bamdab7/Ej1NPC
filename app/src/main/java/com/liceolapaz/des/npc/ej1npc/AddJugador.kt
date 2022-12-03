1d7d3e199553daf09b617cb5c59819d4001e318a
nekoo <nereapena42@gmail.com>
2022-12-03 16:57:31 +0100

    Ajustes visuales + instanciando variables

--- app/src/main/java/com/liceolapaz/des/npc/ej1npc/AddJugador.kt
+++ app/src/main/java/com/liceolapaz/des/npc/ej1npc/AddJugador.kt
@@ -2,10 +2,33 @@ package com.liceolapaz.des.npc.ej1npc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AbsSpinner
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 
 class AddJugador : AppCompatActivity() {
+
+    //Declaramos la funcionalidad del boton aceptar + el de cancelar
+    private lateinit var txtCodigo : EditText
+    private lateinit var txtNombre : EditText
+    private lateinit var txtPrecio : EditText
+    private lateinit var spinner: Spinner
+    private lateinit var txtPuntos : EditText
+    private lateinit var btnAceptar : Button
+    private lateinit var btnCancelar : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_jugador)
+
+        //Instanciamos
+        txtCodigo = findViewById(R.id.txtCodigo)
+        txtNombre = findViewById(R.id.txtNombre)
+        txtPrecio = findViewById(R.id.txtPrecio)
+        spinner = findViewById(R.id.spinnerPosicion)
+        txtPuntos = findViewById(R.id.txtPuntos)
+        btnAceptar = findViewById(R.id.btnAceptar)
+        btnCancelar = findViewById(R.id.btnCancelar)
     }
 }

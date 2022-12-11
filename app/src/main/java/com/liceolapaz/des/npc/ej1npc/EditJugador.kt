7aa96bcc2c681b072365db6656b6033f8c00e6a5
nekoo <nereapena42@gmail.com>
2022-12-11 15:29:09 +0100

    Mostrando el resto de valores del jugador (falta update y delete + dialogos)

--- app/src/main/java/com/liceolapaz/des/npc/ej1npc/EditJugador.kt
+++ app/src/main/java/com/liceolapaz/des/npc/ej1npc/EditJugador.kt
@@ -2,6 +2,8 @@ package com.liceolapaz.des.npc.ej1npc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
@@ -32,6 +34,35 @@ class EditJugador : AppCompatActivity() {
         txtPuntos = findViewById(R.id.txtPuntos)
 
         textNombreJugador.text = intent.getStringExtra("Nombre")
-        
+
+        val codigo = intent.getIntExtra("Codigo",0)
+        txtCodigo.setText(codigo.toString())
+        txtCodigo.isEnabled = false
+
+        val nombre = intent.getStringExtra("Nombre")
+        txtNombre.setText(nombre)
+
+        val precio = intent.getDoubleExtra("Precio",0.0)
+        txtPrecio.setText(precio.toString())
+
+        val position = intent.getStringExtra("Posicion")
+        if(intent.getStringExtra("Posicion") == "Portero"){
+            spinnerPosicion.getItemAtPosition(0)
+            spinnerPosicion.setSelection(0)
+        }else if(intent.getStringExtra("Posicion") == "Defensa"){
+            spinnerPosicion.getItemAtPosition(1)
+            spinnerPosicion.setSelection(1)
+        }else if(intent.getStringExtra("Posicion") == "MedioCampista"){
+            spinnerPosicion.getItemAtPosition(2)
+            spinnerPosicion.setSelection(2)
+        }else if(intent.getStringExtra("Posicion") == "Delantero"){
+            spinnerPosicion.getItemAtPosition(3)
+            spinnerPosicion.setSelection(3)
+        }
+        Log.e("posicion",position.toString())
+
+        val puntos = intent.getIntExtra("Puntos",0)
+        txtPuntos.setText(puntos.toString())
+
     }
 }
